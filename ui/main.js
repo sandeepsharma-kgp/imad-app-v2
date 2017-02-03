82d8432746395e28ac29cca4ca84b4a3b50f4337
Sandeep Sharma <sandeepsharma.iit@gmail.com>
2017-02-03 18:58:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 var element =document.getElementById('main-text'
     );
 element.innerHTML='New Value';
+
+//Move the image
+
+var img=document.getElementById('madi');
+madi.onclick=function(){
+    img.style.marginLeft='100';
+}
