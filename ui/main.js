54451d039b90b13c07caa506e1abcb43c379afb0
Sandeep Sharma <sandeepsharma.iit@gmail.com>
2017-02-04 00:04:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,14 +10,22 @@ var counter=0;
 //console.log('Clicked');
 button.onclick = function(){
     //Make a request to the counter endpoint
-    
+    var request=new XMLHttpRequest();
     //Capture the response and store it in a variable
-    
-    // Render the variable in the correct span
-    counter = counter+1;
-    var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
-    
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE)
+        {
+            if(request.status === 200){
+                var counter=request.reponseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+            }      
+            //Not done yet
+        }
+    };
+    //Make the request
+    request.open('GET','http://sandeepsharma-kgp.imad.hasura-app.io/counter',true);
+    request.send(null);
 
 };
 
