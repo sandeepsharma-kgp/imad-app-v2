3aa4da25d5bb77dc68202fa6aab5f541024ccb32
Sandeep Sharma <sandeepsharma.iit@gmail.com>
2017-02-04 08:13:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ submit.onclick=function(){
     //Capture a list of name and render it is a list
     var name=['name1','name2','name3','name4'];
     var list='';
-    for (var i=0;i<name.length();i++){
+    for (var i=0;i<name.length;i++){
         list+='<li>'+name+'</li>';
     }
     var ul=document.getElemenentById('namelist');
