ebd641b09d6eb136f1ac58a60f757dfc42f2ab71
Sandeep Sharma <sandeepsharma.iit@gmail.com>
2017-02-04 08:32:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 var names=[];
-app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) { //YRL :/submit-nmae/name-xxxx
   //Get the name from the request
   var name=req.params.name;
   
