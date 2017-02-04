e923e8419f858851715c6093772ba42c852c246a
Sandeep Sharma <sandeepsharma.iit@gmail.com>
2017-02-04 08:40:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,11 +15,8 @@ request.onreadystatechange = function(){
          var counter = request.responseText;
          var span = document.getElementById('count');
          span.innerHTML = counter.toString();
-
       }
-
     }
-
        //Not done
 };
 
@@ -35,11 +32,31 @@ var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     //make a request to the server and send the name
     //Capture a list of name and render it is a list
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for (var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+          if(request.status === 200){
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for (var i=0;i<names.length;i++){
+                    list+='<li>'+names[i]+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+          }
+        }
+           //Not done
+    };
+    
+    //make request
+    request.open('GET', 'http://sandeepsharma-kgp.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    };
+    
+    
+    
 };
