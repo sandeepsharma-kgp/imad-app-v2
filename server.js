4f28b4b66fe6f47102c54a05ef8d5b0cf6c90c6a
Sandeep Sharma <sandeepsharma.iit@gmail.com>
2017-02-22 00:14:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var config={
     database:'sandeepsharma-kgp',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:'db-sandeepsharma-kgp-59950'
+    //process.env.DB_PASSWORD
     //'db-sandeepsharma-kgp-59950',
 };
 
