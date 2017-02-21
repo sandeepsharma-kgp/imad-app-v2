6a515bfb34cff89f8e92e491e9807147cea250a3
Sandeep Sharma <sandeepsharma.iit@gmail.com>
2017-02-22 00:17:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/test-db',function(req,res){
             res.status(500).send(err.toString());
         }
         else{
-            res.sned(JSON.stringify(result));
+            res.send(JSON.stringify(result));
         }
     });
     //return a response with result
