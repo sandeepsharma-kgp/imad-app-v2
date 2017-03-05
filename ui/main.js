5c40d6c9fe8b0e5b8991a1b6378ae0984ee13ea1
Sandeep Sharma <sandeepsharma.iit@gmail.com>
2017-03-05 16:00:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ submit.onclick=function(){
     console.log(username);
     console.log(password);
     request.open('POST','http://sandeepsharma-kgp.imad.hasura-app.io/login',true);
-    request.setRequestheader('Content-Type','application/json');
+    request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username: username,password: password}));
     
 };
