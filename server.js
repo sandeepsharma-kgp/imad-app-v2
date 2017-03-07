99d77fba1ab1e249c4128389b39c864c784f9f51
Sandeep Sharma <sandeepsharma.iit@gmail.com>
2017-03-07 22:07:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -152,6 +152,12 @@ app.get('/check-login',function(req,res){
 });
 
 
+app.get('/logout',function(req,res){
+   delete request.session.auth;
+   res.send('Logged Out!!');
+});
+
+
 var pool=new Pool(config);
 
 app.get('/test-db',function(req,res){
